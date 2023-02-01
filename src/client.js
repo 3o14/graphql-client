80e28f4bf3c8928a26b3d6a36881e944e38a0198
3o14 <dldnjswn4265@gmail.com>
2023-02-02 02:58:49 +0900

    라우팅

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/", // 서버
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache(), // 한 번 fetch한 데이터는 캐시에 저장해줌ㄹ
 });
 
 
